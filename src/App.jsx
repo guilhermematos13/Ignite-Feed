041cd49b7059ebdf65315f04f3277fb33764fc9c
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-11-30 21:22:32 -0300

    created header and sidebar profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Header } from "./components/Header";
+import "./global.css";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Guilherme Matos"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto repudiandae corrupti facere delectus illum vero veniam autem mollitia officiis laborum et quasi, consequuntur veritatis quia ullam sit, numquam, rem nemo?"
+          />
+
+          <Post author="Leslie Alexander" content="Um post maneiro !!" />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+import igniteLogo from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do ignite" />
+      <h2>Ignite Feed</h2>
+    </header>
+  );
+}
