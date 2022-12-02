f9c41f1c1f425ca78267e9f97b22a04c6a65973b
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-12-01 23:00:54 -0300

    config sidebar and header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
@@ -8,11 +9,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/guilhermematos13.png"
+        />
         <strong>Guilherme Matos</strong>
         <span> Front-end Developer </span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
