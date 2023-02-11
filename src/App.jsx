3ee186998d1dd6a1636afad46e17b3de5801b9b4
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-02-11 11:30:17 -0300

    feat: iteration with other data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,49 @@ import { Post } from "./components/Post";
 import "./global.css";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/guilhermematos13.png",
+      name: "Guilherme Matos",
+      role: "Front-end Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2023-02-10 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/micaellyuchoa.png",
+      name: "Micaelly Uchoa",
+      role: "Front-end Developer Senior",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2023-02-10 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -13,8 +56,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,15 +2,15 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post(props) {
   return (
     <article className={styles.post}>
       <header className={styles.headerPost}>
         <div className={styles.author}>
-          <Avatar src="https://github.com/guilhermematos13.png" />
+          <Avatar src={props.author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Guilherme Matos</strong>
-            <span>Front-end Developer</span>
+            <strong>{props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
         </div>
 
@@ -18,20 +18,7 @@ export function Post() {
           Públicado há 1h
         </time>
       </header>
-      <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw </a>{" "}
-          <a href="">#rocketseat</a>
-        </p>
-      </div>
+      <div className={styles.content}></div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
