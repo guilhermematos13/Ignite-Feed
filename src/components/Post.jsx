d0f139000ce1f8becb27d1ec5fbd346e8eda2469
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-02-11 13:56:58 -0300

    feat: create delete comments function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,13 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment != commentToDelete;
+    });
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header className={styles.headerPost}>
@@ -81,7 +88,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
